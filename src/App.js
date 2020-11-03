986ae993d143b5d1a6666afb647e3c7033fbe335
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-11-03 15:02:39 +0000

    Added index view and basic navigation header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Index from './components/notebooks/index.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link to={'/'} className="navbar-brand">Scription</Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={'/index'} className="nav-link">Notebooks</Link>
+              </li>
+            </ul>
+          </div>
+        </nav> <br/>
+
+        <h2>Welcome to Scription :)</h2><br/>
+        <Switch>
+          <Route path='/index' component={ Index } />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
