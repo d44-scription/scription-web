b0c21131590255ce36d322a75c428641efb777a5
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-11-03 15:20:53 +0000

    Added axios. Successful connection to Ghibli API

--- src/components/notebooks/index.component.js
+++ src/components/notebooks/index.component.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TableRow from './TableRow';
 
 export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { notebook: [] };
+  }
+
+  componentDidMount() {
+    axios.get('https://ghibliapi.herokuapp.com/films')
+      .then(response => {
+        this.setState({ notebook: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
+  tabRow() {
+    return this.state.notebook.map(function (object, i) {
+      return <TableRow obj={object} key={i} />;
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>Index View</p>
+        <h3 align="center">Notebook List</h3>
+        <table className="table table-striped" style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Notebook</th>
+              <th>ID</th>
+              <th colSpan="2">Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.tabRow()}
+          </tbody>
+        </table>
       </div>
-    )
+    );
   }
 }
