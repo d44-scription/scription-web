7d51a17ea7a219770d73cf4d7fff8dd0c59f2bde
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-11-09 16:32:09 +0000

    Upgraded bootstrap to v5. added delete functionality

--- src/components/notebooks/index.component.js
+++ src/components/notebooks/index.component.js
@@ -8,6 +8,7 @@ export default class Index extends Component {
 
     this.retrieveNotebooks = this.retrieveNotebooks.bind(this);
     this.setActiveNotebook = this.setActiveNotebook.bind(this);
+    this.deleteNotebook = this.deleteNotebook.bind(this);
 
     this.state = {
       notebooks: [],
@@ -39,6 +40,23 @@ export default class Index extends Component {
     });
   }
 
+  deleteNotebook(id) {
+    let notebooks = this.state.notebooks.slice();
+    let remainingNotebooks = notebooks.filter(nb => (nb.id !== id))
+
+    NotebookDataService.delete(id)
+      .then(() => {
+        this.setState({
+          notebooks: remainingNotebooks,
+          currentIndex: -1,
+          currentId: null,
+        })
+      })
+      .catch((e) => {
+        console.log(e)
+      });
+  }
+
   render() {
     const { notebooks, currentIndex, currentId } = this.state;
 
@@ -51,10 +69,26 @@ export default class Index extends Component {
               notebooks.map((notebook, index) => (
                 <li
                   key={index}
-                  className={`list-group-item ${(index === currentIndex ? "active" : "")}`}
+                  className={`list-group-item d-inline-flex justify-content-between ${(index === currentIndex ? "active" : "")}`}
                   onClick={() => this.setActiveNotebook(notebook.id, index)}
                 >
-                  {notebook.name}
+                  <p>
+                    {notebook.name}
+                  </p>
+
+                  <p>
+                    <svg
+                      onClick={() => this.deleteNotebook(notebook.id)}
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 16 16"
+                      className="bi bi-archive"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path fillRule="evenodd" d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
+                    </svg>
+                  </p>
                 </li>
               ))}
           </ul>
