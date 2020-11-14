ff1329c6535dba5266c83021f5f79a8cb959ddc6
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-11-14 17:36:18 +0000

    Added busy state to component

--- src/components/inline-editor.component.js
+++ src/components/inline-editor.component.js
@@ -6,6 +6,7 @@ import "../css/inline-editor.css"
 function InlineEditor(props) {
   // Define callbacks for GETting and SETting the text and rest state of the component
   const [atRest, setAtRest] = useState(true);
+  const [isBusy, setIsBusy] = useState(false);
   const [value, setValue] = useState(props.value);
 
   const inputRef = useRef(null);
@@ -22,8 +23,9 @@ function InlineEditor(props) {
   useKeypress('Escape', () => {
     if (document.activeElement === inputRef.current) {
       setAtRest(true)
+      setValue(props.value)
     }
-  }, [atRest, setAtRest]);
+  }, [atRest, setAtRest, setValue]);
 
   // Callback for enter key - save & exit (*only if the textbox is focused)
   useKeypress('Enter', () => {
@@ -39,12 +41,21 @@ function InlineEditor(props) {
     }
   }, [setAtRest, value, atRest]);
 
+  // Function to submit data & return to rest state
   const saveAndExit = useCallback(() => {
     const { id, model, param } = props
     setAtRest(true)
+    setIsBusy(true)
 
     NotebookDataService.update(id, model, param, value)
-  }, [value, props]);
+      .then(response => {
+        setIsBusy(false)
+      })
+      .catch(e => {
+        setIsBusy(false)
+        console.log(e)
+      });
+  }, [value, props, setIsBusy]);
 
   // Set focus to the text field when shown
   useEffect(() => {
@@ -75,7 +86,18 @@ function InlineEditor(props) {
         onChange={onChange}
         onBlur={onBlur}
         className={`inline-input ${atRest ? 'hidden' : ''}`}
+        disabled={isBusy ? true : false}
       />
+
+      <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 16 16"
+        className={`bi bi-hourglass-split ${isBusy ? '' : 'hidden'}`}
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0c0 .701.478 1.236 1.011 1.492A3.5 3.5 0 0 1 11.5 13s-.866-1.299-3-1.48V8.35z" />
+      </svg>
     </span>
   );
 }
