600d910dc516b769ccb1bfe0b682c535dd640ad3
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-11-14 15:42:19 +0000

    Added comments to code. Added callback to set focus to the input when it is shown

--- src/components/inline-editor.component.js
+++ src/components/inline-editor.component.js
@@ -1,11 +1,15 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useRef, useEffect } from "react";
 import NotebookDataService from "../services/notebook.service";
 import "../css/inline-editor.css"
 
 function InlineEditor(props) {
+  // Define callbacks for GETting and SETting the text and rest state of the component
   const [atRest, setAtRest] = useState(true);
   const [value, setValue] = useState(props.displayValue);
 
+  const inputRef = useRef(null);
+
+  // Callback(/event handler) for when text is changed
   const onChange = useCallback(
     event => {
       setValue(event.target.value);
@@ -13,11 +17,20 @@ function InlineEditor(props) {
     [setValue]
   );
 
+  // Callback for when text input is blurred
   const onBlur = useCallback(
     () => setAtRest(true), [
     setAtRest
   ]);
 
+  // Set focus to the text field when shown
+  useEffect(() => {
+    if (!atRest) {
+      inputRef.current.focus();
+    }
+  }, [atRest]);
+
+  // Callback to update the rest state when the text span is clicked
   const onSpanClick = useCallback(
     () => setAtRest(false), [
     setAtRest
@@ -33,8 +46,9 @@ function InlineEditor(props) {
       </span>
 
       <input
+        ref={inputRef}
         type="text"
-        value={value || ''}
+        value={value || props.displayValue || ''}
         onChange={onChange}
         onBlur={onBlur}
         className={`inline-input ${atRest ? 'hidden' : ''}`}
