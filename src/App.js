c80bf9cfa6d2471623371aec9f5aab5f84d87f87
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-11-18 16:11:50 +0000

    Removed legacy components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import Index from './components/notebooks/index.component';
-import LegacyIndex from './components/legacy_notebooks/index.component';
-import LegacyNotebook from './components/legacy_notebooks/notebook.component';
 
 function App() {
   return (
@@ -18,21 +16,12 @@ function App() {
               Notebooks
             </Link>
           </li>
-
-          <li className="nav-item">
-            <Link to={"/legacy_notebooks"} className="nav-link">
-              Legacy Notebooks
-            </Link>
-          </li>
         </div>
       </nav>
 
       <div className="container mt-3">
         <Switch>
           <Route exact path={["/", "/notebooks"]} component={Index} />
-
-          <Route exact path={["/legacy_notebooks"]} component={LegacyIndex} />
-          <Route path="/legacy_notebooks/:id" component={LegacyNotebook} />
         </Switch>
       </div>
     </div>
