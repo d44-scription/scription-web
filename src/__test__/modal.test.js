05c477f82743127d1cf83c0a32e27d7fe18cbf8f
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-11-23 16:08:45 +0000

    Added user event library. Expanded modal specs to confirm function runs and modal visibility

--- src/__test__/modal.test.js
+++ src/__test__/modal.test.js
@@ -1,7 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import ConfirmModal from '../components/modal.component'
+import userEvent from '@testing-library/user-event'
 
-test('renders navigation bar', () => {
+test('shows and hides modal when prop changes', () => {
+  const { rerender } = render(
+    <ConfirmModal
+      visible={false}
+    ></ConfirmModal>
+  );
+
+  let titleText = screen.queryByText(/Are you sure\?/i);
+  expect(titleText).not.toBeInTheDocument();
+
+  rerender(
+    <ConfirmModal
+      visible={true}
+    ></ConfirmModal>
+  );
+
+  titleText = screen.queryByText(/Are you sure\?/i);
+  expect(titleText).toBeInTheDocument();
+});
+
+test('renders custom modal text', () => {
   render(
     <ConfirmModal
       visible={true}
@@ -15,4 +36,40 @@ test('renders navigation bar', () => {
 
   expect(titleText).toBeInTheDocument();
   expect(bodyText).toBeInTheDocument();
+});
+
+test('successfully runs prop on close', () => {
+  let testVal = false
+
+  render(
+    <ConfirmModal
+      visible={true}
+      closeAction={() => testVal = true}
+    ></ConfirmModal>
+  );
+
+  // Sanity check
+  expect(testVal).toBeFalsy()
+
+  userEvent.click(screen.getByText('Cancel'))
+
+  expect(testVal).toBeTruthy()
+});
+
+test('successfully runs prop on confirm', () => {
+  let testVal = false
+
+  render(
+    <ConfirmModal
+      visible={true}
+      confirmAction={() => testVal = true}
+    ></ConfirmModal>
+  );
+
+  // Sanity check
+  expect(testVal).toBeFalsy()
+
+  userEvent.click(screen.getByText('OK'))
+
+  expect(testVal).toBeTruthy()
 });
