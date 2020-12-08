38003dbbf831ef0ea7b8c306cd2bcbea480deed7
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-12-08 11:42:30 +0000

    Updated text component tests

--- src/__test__/inline-editors/text.test.js
+++ src/__test__/inline-editors/text.test.js
@@ -4,131 +4,134 @@ import { act } from "react-dom/test-utils";
 import http from "../../http-common";
 import userEvent from "@testing-library/user-event";
 
-it("correctly enters and leaves rest state", async () => {
-  let value = "Test Text";
+describe("Text component", () => {
+  const value = "Test Text";
   const successfulResponse = {
     code: 200,
   };
 
-  jest.spyOn(http, "put").mockImplementation(() =>
-    Promise.resolve({
-      data: successfulResponse,
-    })
-  );
-
-  // Use the asynchronous version of act to apply resolved promises
-  await act(async () => {
-    render(<Text value={value} />);
+  beforeEach(() => {
+    jest.spyOn(http, "put").mockImplementation(() =>
+      Promise.resolve({
+        data: successfulResponse,
+      })
+    );
   });
 
-  // Confirm that, at rest state, text is visible, text box & spinner are hidden
-  expect(screen.getByText(value)).toBeVisible();
-  expect(screen.queryByRole("textbox")).toBeNull();
-  expect(screen.getByTitle("Saving changes")).not.toBeVisible();
-
-  // Click span
-  userEvent.click(screen.getByText(value));
-
-  // Confirm that when not at rest state, text box is visible, text & spinner are hidden
-  expect(screen.queryByText(value)).not.toBeVisible();
-  expect(screen.getByRole("textbox")).toBeVisible();
-  expect(screen.queryByTitle("Saving changes")).not.toBeVisible();
-
-  // Press `enter`
-  await act(async () => {
-    userEvent.type(screen.getByRole("textbox"), "{enter}");
+  afterEach(() => {
+    http.put.mockRestore();
   });
 
-  // Confirm that we have returned to rest state
-  expect(screen.getByText(value)).toBeVisible();
-  expect(screen.queryByRole("textbox")).toBeNull();
-  expect(screen.getByTitle("Saving changes")).not.toBeVisible();
-
-  // Click span, press escape
-  userEvent.click(screen.getByText(value));
-
-  await act(async () => {
-    userEvent.type(screen.getByRole("textbox"), "{esc}");
-  });
-
-  // Confirm that we have returned to rest state
-  expect(screen.getByText(value)).toBeVisible();
-  expect(screen.queryByRole("textbox")).toBeNull();
-  expect(screen.getByTitle("Saving changes")).not.toBeVisible();
-});
-
-it("renders with a given font size", async () => {
-  let value = "Test Text";
-  let fontSize = "2rem";
-
-  // Use the asynchronous version of act to apply resolved promises
-  await act(async () => {
-    render(<Text value={value} fontSize={fontSize} />);
-  });
-
-  // Confirm span has correct font size
-  expect(
-    screen.getByText(value).style.cssText.includes("font-size: 2rem")
-  ).toBe(true);
-
-  // Click span
-  userEvent.click(screen.getByText(value));
-
-  // Confirm text field has correct font size
-  expect(
-    screen.getByRole("textbox").style.cssText.includes("font-size: 2rem")
-  ).toBe(true);
-});
-
-it("renders with a default font size", async () => {
-  let value = "Test Text";
-
-  // Use the asynchronous version of act to apply resolved promises
-  await act(async () => {
-    render(<Text value={value} />);
-  });
-
-  // Confirm span has correct font size
-  expect(
-    screen.getByText(value).style.cssText.includes("font-size: 1rem")
-  ).toBe(true);
-
-  // Click span
-  userEvent.click(screen.getByText(value));
-
-  // Confirm text field has correct font size
-  expect(
-    screen.getByRole("textbox").style.cssText.includes("font-size: 1rem")
-  ).toBe(true);
-});
-
-// FIXME: This is a hacky workaround, but the input automatically gains focus in CI tests.
-// In CI tests, this test is essentially useless as it just focuses the element and confirms
-// it leaves rest state, as tested above. But the tests pass locally
-if (!process.env.CI) {
-  it("responds to tab correctly", async () => {
-    let value = "Test Text";
-
+  test("entering and leaving rest state", async () => {
     // Use the asynchronous version of act to apply resolved promises
     await act(async () => {
       render(<Text value={value} />);
     });
 
-    userEvent.tab();
-
-    // Confirm that span has focus
-    const span = screen.getByText(value);
-    expect(document.activeElement).toEqual(span);
-
-    // Confirm we are in rest state
-    expect(span).toBeVisible();
+    // Confirm that, at rest state, text is visible, text box & spinner are hidden
+    expect(screen.getByText(value)).toBeVisible();
     expect(screen.queryByRole("textbox")).toBeNull();
+    expect(screen.getByTitle("Saving changes")).not.toBeVisible();
 
-    // Press enter on focused element
-    userEvent.type(span, "{enter}", { skipClick: true });
+    // Click span
+    userEvent.click(screen.getByText(value));
 
-    // Confirm that we have left rest state
+    // Confirm that when not at rest state, text box is visible, text & spinner are hidden
     expect(screen.queryByText(value)).not.toBeVisible();
     expect(screen.getByRole("textbox")).toBeVisible();
+    expect(screen.queryByTitle("Saving changes")).not.toBeVisible();
+
+    // Press `enter`
+    await act(async () => {
+      userEvent.type(screen.getByRole("textbox"), "{enter}");
+    });
+
+    // Confirm that we have returned to rest state
+    expect(screen.getByText(value)).toBeVisible();
+    expect(screen.queryByRole("textbox")).toBeNull();
+    expect(screen.getByTitle("Saving changes")).not.toBeVisible();
+
+    // Click span, press escape
+    userEvent.click(screen.getByText(value));
+
+    await act(async () => {
+      userEvent.type(screen.getByRole("textbox"), "{esc}");
+    });
+
+    // Confirm that we have returned to rest state
+    expect(screen.getByText(value)).toBeVisible();
+    expect(screen.queryByRole("textbox")).toBeNull();
+    expect(screen.getByTitle("Saving changes")).not.toBeVisible();
   });
-}
+
+  test("rendering with a given font size", async () => {
+    let fontSize = "2rem";
+
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<Text value={value} fontSize={fontSize} />);
+    });
+
+    // Confirm span has correct font size
+    expect(
+      screen.getByText(value).style.cssText.includes("font-size: 2rem")
+    ).toBe(true);
+
+    // Click span
+    userEvent.click(screen.getByText(value));
+
+    // Confirm text field has correct font size
+    expect(
+      screen.getByRole("textbox").style.cssText.includes("font-size: 2rem")
+    ).toBe(true);
+  });
+
+  test("rendering with a default font size", async () => {
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<Text value={value} />);
+    });
+
+    // Confirm span has correct font size
+    expect(
+      screen.getByText(value).style.cssText.includes("font-size: 1rem")
+    ).toBe(true);
+
+    // Click span
+    userEvent.click(screen.getByText(value));
+
+    // Confirm text field has correct font size
+    expect(
+      screen.getByRole("textbox").style.cssText.includes("font-size: 1rem")
+    ).toBe(true);
+  });
+
+  // FIXME: This is a hacky workaround, but the input automatically gains focus in CI tests.
+  // In CI tests, this test is essentially useless as it just focuses the element and confirms
+  // it leaves rest state, as tested above. But the tests pass locally
+  if (!process.env.CI) {
+    test("responding to tab", async () => {
+      // Use the asynchronous version of act to apply resolved promises
+      await act(async () => {
+        render(<Text value={value} />);
+      });
+
+      userEvent.tab();
+
+      // Confirm that span has focus
+      const span = screen.getByText(value);
+      expect(document.activeElement).toEqual(span);
+
+      // Confirm we are in rest state
+      expect(span).toBeVisible();
+      expect(screen.queryByRole("textbox")).toBeNull();
+
+      // Press enter on focused element
+      userEvent.type(span, "{enter}", { skipClick: true });
+
+      // Confirm that we have left rest state
+      expect(screen.queryByText(value)).not.toBeVisible();
+      expect(screen.getByRole("textbox")).toBeVisible();
+    });
+  }
+});
