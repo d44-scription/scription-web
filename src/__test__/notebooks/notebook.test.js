5736b6e607b02f450ee8ef96134e3ca1af447fc1
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-12-08 11:33:24 +0000

    Updated notebook component specs

--- src/__test__/notebooks/notebook.test.js
+++ src/__test__/notebooks/notebook.test.js
@@ -3,7 +3,7 @@ import Notebook from "../../components/notebooks/notebook.component";
 import { act } from "react-dom/test-utils";
 import http from "../../http-common";
 
-it("renders information for a given notebook", async () => {
+describe("Notebook component", () => {
   const fakeNotebook = {
     name: "Notebook 1",
     id: 1,
@@ -11,20 +11,26 @@ it("renders information for a given notebook", async () => {
     order_index: 0,
   };
 
-  jest.spyOn(http, "get").mockImplementation(() =>
-    Promise.resolve({
-      data: fakeNotebook,
-    })
-  );
-
-  // Use the asynchronous version of act to apply resolved promises
-  await act(async () => {
-    render(<Notebook id="1" />);
+  beforeEach(() => {
+    jest.spyOn(http, "get").mockImplementation(() =>
+      Promise.resolve({
+        data: fakeNotebook,
+      })
+    );
   });
 
-  // Confirm data is retrieved and displayed correctly
-  expect(screen.getByText("Notebook 1")).toBeInTheDocument();
-  expect(screen.getAllByText("Mock summary")[0]).toBeInTheDocument();
+  afterEach(() => {
+    http.get.mockRestore();
+  });
 
-  http.get.mockRestore();
+  it("renders information for a given notebook", async () => {
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<Notebook id="1" />);
+    });
+
+    // Confirm data is retrieved and displayed correctly
+    expect(screen.getByText("Notebook 1")).toBeInTheDocument();
+    expect(screen.getAllByText("Mock summary")[0]).toBeInTheDocument();
+  });
 });
