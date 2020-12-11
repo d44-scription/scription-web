af307363fc15c6788df1c730b1bbb12a607b9437
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-12-11 15:39:59 +0000

    Re-added tests

---
+++ src/__test__/notebooks/edit.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import Edit from "../../components/notebooks/edit.component";
+import { act } from "react-dom/test-utils";
+import http from "../../http-common";
+
+describe("Notebook component", () => {
+  const fakeNotebook = {
+    name: "Notebook 1",
+    id: 1,
+    summary: "Mock summary",
+    order_index: 0,
+  };
+
+  beforeEach(() => {
+    jest.spyOn(http, "get").mockImplementation(() =>
+      Promise.resolve({
+        data: fakeNotebook,
+      })
+    );
+  });
+
+  afterEach(() => {
+    http.get.mockRestore();
+  });
+
+  test("rendering information for a given notebook", async () => {
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<Edit id="1" />);
+    });
+
+    // Confirm data is retrieved and displayed correctly
+    expect(screen.getByText("Notebook 1")).toBeInTheDocument();
+    expect(screen.getAllByText("Mock summary")[0]).toBeInTheDocument();
+  });
+});
