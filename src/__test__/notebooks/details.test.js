46c104ed6fcf744f2f927ec06505834c396864fd
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-12-15 11:33:48 +0000

    Added test for when details tab is given an id

--- src/__test__/notebooks/details.test.js
+++ src/__test__/notebooks/details.test.js
@@ -2,19 +2,49 @@ import { render, screen } from "@testing-library/react";
 import Details from "../../components/notebooks/details.component";
 import { act } from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event";
+import http from "../../http-common";
 
 describe("Details component", () => {
   const confirmRestState = () => {
-    // Confirm default button is only thing rendered
+    // Confirm default button is shown
     expect(screen.getByText("Add new notebook")).toBeInTheDocument();
+
+    // Confirm new notebook fields are not shown
     expect(screen.queryByText("Name notebook")).toBeNull();
     expect(screen.queryByText("Cancel")).toBeNull();
-  }
+
+    // Confirm view notebook fields are not shown
+    expect(screen.queryByText("Notebook 1")).toBeNull();
+    expect(screen.queryByText("Mock summary")).toBeNull();
+    expect(screen.queryByText("Delete notebook")).toBeNull();
+  };
 
   const confirmNewNotebookState = () => {
+    // Confirm default button is not shown
     expect(screen.queryByText("Add new notebook")).not.toBeInTheDocument();
+
+    // Confirm new notebook fields are shown
     expect(screen.getByText("Name notebook")).toBeInTheDocument();
     expect(screen.getByText("Cancel")).toBeInTheDocument();
+
+    // Confirm view notebook fields are not shown
+    expect(screen.queryByText("Notebook 1")).toBeNull();
+    expect(screen.queryByText("Mock summary")).toBeNull();
+    expect(screen.queryByText("Delete notebook")).toBeNull();
+  };
+
+  const confirmViewNotebookState = () => {
+    // Confirm default button is not shown
+    expect(screen.queryByText("Add new notebook")).not.toBeInTheDocument();
+
+    // Confirm new notebook fields are not shown
+    expect(screen.queryByText("Name notebook")).toBeNull();
+    expect(screen.queryByText("Cancel")).toBeNull();
+
+    // Confirm view notebook fields are shown
+    expect(screen.getByText("Notebook 1")).toBeInTheDocument();
+    expect(screen.getAllByText("Mock summary")[0]).toBeInTheDocument();
+    expect(screen.getByText("Delete notebook")).toBeInTheDocument();
   }
 
   test("rendering details pane when no ID given", async () => {
@@ -40,5 +70,31 @@ describe("Details component", () => {
 
     // Confirm we return to rest state
     confirmRestState();
+  });
+
+  describe("When given an ID", () => {
+    const fakeNotebook = {
+      name: "Notebook 1",
+      id: 1,
+      summary: "Mock summary",
+      order_index: 0,
+    };
+
+    beforeEach(() => {
+      jest.spyOn(http, "get").mockImplementation(() =>
+        Promise.resolve({
+          data: fakeNotebook,
+        })
+      );
+    });
+
+    test("correctly viewing notebook page", async () => {
+      await act(async () => {
+        render(<Details id={1} />);
+      });
+
+      // Confirm edit page is shown correctly
+      confirmViewNotebookState();
+    })
   });
 });
