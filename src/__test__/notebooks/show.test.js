e084836419bc50211b931e0297f67b4fe6ff6d2e
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-12-22 12:15:45 +0000

    Added tests to confirm success message shows

--- src/__test__/notebooks/show.test.js
+++ src/__test__/notebooks/show.test.js
@@ -5,18 +5,29 @@ import { act } from "react-dom/test-utils";
 import http from "../../http-common";
 import userEvent from "@testing-library/user-event";
 
-describe("New component", () => {
+describe("Show component", () => {
   const id = 1;
+  const placeholder = "Click here to add a note";
 
   const fakeNotebook = {
       name: "Notebook 1",
       id: id,
   };
 
+  const successfulResponse = {
+    code: 200,
+  };
+
   beforeEach(async () => {
     jest.spyOn(http, "get").mockImplementation(() =>
       Promise.resolve({
         data: fakeNotebook,
+      })
+    );
+
+    jest.spyOn(http, "post").mockImplementation(() =>
+      Promise.resolve({
+        data: successfulResponse,
       })
     );
 
@@ -33,20 +44,40 @@ describe("New component", () => {
 
   afterEach(() => {
     http.get.mockRestore();
+    http.post.mockRestore();
   });
 
   const confirmRestState = () => {
-    expect(screen.getByText("Notebook 1")).toBeInTheDocument();
-    expect(screen.getByText("Click here to add a note")).toBeInTheDocument();
+    expect(screen.getByText("Notebook 1")).toBeVisible();
+    expect(screen.getByText(placeholder)).toBeVisible();
     expect(
       screen.getByText(
         "Use @ to reference a character, : to reference an item, and # to reference a location"
       )
-    ).toBeInTheDocument();
+    ).toBeVisible();
   };
 
-  test("rendering correct fields", async () => {
+  test("displaying success messages", async () => {
     // Confirm we start at rest state
     confirmRestState();
+
+    // Add a new note
+    userEvent.click(screen.getByText(placeholder));
+    await act(async () => {
+      userEvent.type(screen.getByRole("textbox"), "{enter}");
+    });
+
+    // Confirm we have returned to rest state with a success message
+    expect(screen.getByText("Your note has been added")).toBeVisible();
+    confirmRestState();
+
+    // Type another note
+    userEvent.click(screen.getByText(placeholder));
+    await act(async () => {
+      userEvent.type(screen.getByRole("textbox"), "A");
+    });
+
+    // Confirm success message disappears
+    expect(screen.queryByText("Your note has been added")).toBeNull();
   });
 });
