def792a50c5faa1f5a3d43373a5a550d8d94865d
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-12-28 11:32:11 +0000

    Handled navigation programmatically

--- src/components/notebooks/show.component.js
+++ src/components/notebooks/show.component.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import NotebookDataService from "../../services/notebook.service";
+import Button from "react-bootstrap/Button";
 import NoteDataService from "../../services/note.service";
 import InlineEditor from "../inline_editor.component";
 import Person from "../icons/person.component";
@@ -9,12 +10,15 @@ import Gem from "../icons/gem.component";
 import "../../scss/show.scss";
 
 function Show(props) {
+  // ID of notebook to show, taken from params
   const { id } = useParams();
 
   // Define callbacks for GETting and SETting the component state
   const [name, setName] = useState("");
   const [content, setContent] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
+
+  const history = useHistory();
 
   // Callback to update the displayed notebook
   const retrieveNotebook = useCallback(() => {
@@ -44,11 +48,17 @@ function Show(props) {
     return NoteDataService.create(content, id);
   };
 
+  // Empty text box when note added and display success message
   const clearContent = (response) => {
     setContent(null);
 
     // TODO: Retrieve success message from response
     setSuccessMessage("Your note has been added");
+  };
+
+  // Programmatically handle navigation to support accessible buttons
+  const viewNotables = (type) => {
+    history.push(`/notebooks/${id}/${type}`);
   };
 
   return (
@@ -74,17 +84,32 @@ function Show(props) {
         <h1>Notables</h1>
 
         <section className="d-inline-flex justify-content-between w-100">
-          <Link to={`/notebooks/${id}/characters`}>
+          <Button
+            variant="link"
+            onClick={() => {
+              viewNotables("characters");
+            }}
+          >
             <Person title={`View characters for ${name}`} />
-          </Link>
+          </Button>
 
-          <Link to={`/notebooks/${id}/locations`}>
+          <Button
+            variant="link"
+            onClick={() => {
+              viewNotables("locations");
+            }}
+          >
             <House title={`View locations for ${name}`} />
-          </Link>
+          </Button>
 
-          <Link to={`/notebooks/${id}/items`}>
+          <Button
+            variant="link"
+            onClick={() => {
+              viewNotables("items");
+            }}
+          >
             <Gem title={`View items for ${name}`} />
-          </Link>
+          </Button>
         </section>
 
         <h1>Unlinked Notes</h1>
