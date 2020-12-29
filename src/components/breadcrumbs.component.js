0f913d70bd7f0fc7192281dfe4b27a25d79758b9
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-12-29 14:31:00 +0000

    Updated breadcrumbs to respond to tab

--- src/components/breadcrumbs.component.js
+++ src/components/breadcrumbs.component.js
@@ -1,19 +1,27 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 
 function Breadcrumbs(props) {
   const location = useLocation();
+  const history = useHistory();
+
   let paths = location.pathname.split("/");
 
   // At root of website, second path is an empty string
   // Remove this to correctly make root link "active"
   paths = paths[1] === "" ? paths.slice(1) : paths;
 
+  const onKeyDown = (e) => {
+    if (e.key === " " || e.key === "Enter") {
+      history.push(e.target.pathname);
+    }
+  }
+
   const crumbs = paths.map((path, index) => {
     // Replace first path ('') with root link & branding
-    var url = index == 0 ? "/" : `${paths.slice(0, index + 1).join("/")}`;
-    var label = index == 0 ? "Scription" : path;
+    var url = index === 0 ? "/" : `${paths.slice(0, index + 1).join("/")}`;
+    var label = index === 0 ? "Scription" : path;
 
     return (
       <Breadcrumb.Item
@@ -21,6 +29,7 @@ function Breadcrumbs(props) {
         href={url}
         key={index}
         active={index + 1 === paths.length}
+        onKeyDown={onKeyDown}
       >
         {label}
       </Breadcrumb.Item>
