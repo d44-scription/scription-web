64afee0e3d7af32e5918fe047ac0c638e8e900ca
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-12-29 13:41:01 +0000

    Neatened breadcrumb code

--- src/components/breadcrumbs.component.js
+++ src/components/breadcrumbs.component.js
@@ -11,33 +11,20 @@ function Breadcrumbs(props) {
   paths = paths[1] === "" ? paths.slice(1) : paths;
 
   const crumbs = paths.map((path, index) => {
-    var url = `${paths.slice(0, index + 1).join("/")}`;
+    // Replace first path ('') with root link & branding
+    var url = index == 0 ? "/" : `${paths.slice(0, index + 1).join("/")}`;
+    var label = index == 0 ? "Scription" : path;
 
-    if (index === 0) {
-      // First breadcrumb returns a brand link
-      return (
-        <Breadcrumb.Item
-          className="capitalise"
-          key={index}
-          href="/"
-          active={index + 1 === paths.length}
-        >
-          Scription
-        </Breadcrumb.Item>
-      );
-    } else {
-      // Otherwise return a breadcrumb where the last one in the list is active
-      return (
-        <Breadcrumb.Item
-          className="capitalise"
-          href={url}
-          key={index}
-          active={index + 1 === paths.length}
-        >
-          {path}
-        </Breadcrumb.Item>
-      );
-    }
+    return (
+      <Breadcrumb.Item
+        className="capitalise"
+        href={url}
+        key={index}
+        active={index + 1 === paths.length}
+      >
+        {label}
+      </Breadcrumb.Item>
+    );
   });
 
   return <Breadcrumb>{crumbs}</Breadcrumb>;
