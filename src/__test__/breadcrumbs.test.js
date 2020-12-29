6951b222fb7276c78306a83b6341eb6f13a06f1f
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-12-29 15:07:48 +0000

    Added tests to confirm tab functionality of nav bar

--- src/__test__/breadcrumbs.test.js
+++ src/__test__/breadcrumbs.test.js
@@ -1,46 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import Breadcrumbs from "../components/breadcrumbs.component";
 import { MemoryRouter, Route } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
-describe("Show component", () => {
-  test("displaying each breadcrumb correctly", () => {
-    const path = "notebooks/path/to/resource";
-
-    render(
-      <MemoryRouter initialEntries={[path]}>
-        <Route path={path}>
-          <Breadcrumbs />
-        </Route>
-      </MemoryRouter>
-    );
-
-    // First crumb ("/notebooks") replaced with brand
-    const brandLink = screen.getByText("Scription");
-    const notebooksCrumb = screen.queryByText("notebooks");
-    const pathCrumb = screen.getByText("path");
-    const toCrumb = screen.getByText("to");
-    const resourceCrumb = screen.getByText("resource");
-
-    // Confirm all links rendered
-    expect(brandLink).toBeVisible();
-    expect(notebooksCrumb).toBeNull();
-    expect(pathCrumb).toBeVisible();
-    expect(toCrumb).toBeVisible();
-    expect(resourceCrumb).toBeVisible();
-
-    // Confirm only latest item is active
-    expect(brandLink).not.toHaveClass("active");
-    expect(pathCrumb).not.toHaveClass("active");
-    expect(toCrumb).not.toHaveClass("active");
-    expect(resourceCrumb).toHaveClass("active");
-
-    // Confirm links are generated correctly
-    expect(brandLink).toHaveAttribute("href", "/");
-    expect(pathCrumb).toHaveAttribute("href", "notebooks/path");
-    expect(toCrumb).toHaveAttribute("href", "notebooks/path/to");
-    expect(resourceCrumb).not.toHaveAttribute("href");
-  });
-
+describe("Breadcrumbs component", () => {
   test("displaying brand in isolation correctly", () => {
     render(
       <MemoryRouter initialEntries={["/"]}>
@@ -57,5 +20,71 @@ describe("Show component", () => {
     expect(brandLink).toHaveClass("active");
     expect(brandLink).not.toHaveAttribute("href");
     expect(screen.queryByText("/")).toBeNull();
+  });
+
+  describe("When generating multiple crumbs", () => {
+    const path = "notebooks/path/to/resource";
+
+    beforeEach(() => {
+      render(
+        <MemoryRouter initialEntries={[path]}>
+          <Route path={path}>
+            <Breadcrumbs />
+          </Route>
+        </MemoryRouter>
+      );
+    });
+
+    test("displaying each breadcrumb correctly", () => {
+      // First crumb ("/notebooks") replaced with brand
+      const brandLink = screen.getByText("Scription");
+      const notebooksCrumb = screen.queryByText("notebooks");
+      const pathCrumb = screen.getByText("path");
+      const toCrumb = screen.getByText("to");
+      const resourceCrumb = screen.getByText("resource");
+
+      // Confirm all links rendered
+      expect(brandLink).toBeVisible();
+      expect(notebooksCrumb).toBeNull();
+      expect(pathCrumb).toBeVisible();
+      expect(toCrumb).toBeVisible();
+      expect(resourceCrumb).toBeVisible();
+
+      // Confirm only latest item is active
+      expect(brandLink).not.toHaveClass("active");
+      expect(pathCrumb).not.toHaveClass("active");
+      expect(toCrumb).not.toHaveClass("active");
+      expect(resourceCrumb).toHaveClass("active");
+
+      // Confirm links are generated correctly
+      expect(brandLink).toHaveAttribute("href", "/");
+      expect(pathCrumb).toHaveAttribute("href", "notebooks/path");
+      expect(toCrumb).toHaveAttribute("href", "notebooks/path/to");
+      expect(resourceCrumb).not.toHaveAttribute("href");
+    });
+
+    test("responding to tab+enter correctly", () => {
+      const brandLink = screen.getByText("Scription");
+
+      userEvent.tab();
+
+      expect(brandLink).toHaveFocus();
+
+      userEvent.type(brandLink, "{enter}", { skipClick: true });
+
+      expect(brandLink).not.toBeInTheDocument();
+    });
+
+    test("responding to tab+space correctly", () => {
+      const brandLink = screen.getByText("Scription");
+
+      userEvent.tab();
+
+      expect(brandLink).toHaveFocus();
+
+      userEvent.type(brandLink, " ", { skipClick: true });
+
+      expect(brandLink).not.toBeInTheDocument();
+    });
   });
 });
