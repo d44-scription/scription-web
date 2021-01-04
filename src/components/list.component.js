d1a976a5d3542f458f231364f5f91bb834bb84ed
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-01-04 15:39:08 +0000

    Started styling search bar

--- src/components/list.component.js
+++ src/components/list.component.js
@@ -47,11 +47,25 @@ function List(props) {
 
   return (
     <div>
-      <Form.Control
-        placeholder="Search list"
-        value={query}
-        onChange={onChange}
-      />
+      <span className="w-100 d-inline-flex align-items-start">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          className="bi bi-search m-1"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+
+        <Form.Control
+          placeholder="Search list"
+          className="search-field"
+          value={query}
+          onChange={onChange}
+        />
+      </span>
 
       <p hidden={queriedItems.length > 0 || query === ""}>
         No search results found
