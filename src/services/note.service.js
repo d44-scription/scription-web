2fb97e1123515b4ccbcf3bd6ea927dc9096b52cf
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-01 14:37:45 +0000

    Added new action to note service. Updated mentionable to respond to new props

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -12,6 +12,14 @@ class NoteDataService {
 
     return http.post(`/notebooks/${notebookId}/notes.json`, params);
   }
+
+  update(notebookId, id, param, value) {
+    let params = {};
+    params["note"] = {};
+    params["note"][param] = value;
+
+    return http.put(`/notebooks/${notebookId}/notes/${id}.json`, params);
+  }
 }
 
 export default new NoteDataService();
