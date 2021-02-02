ce445f6d88b4eb4ab7940ca3461695fc93a5e7ad
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-02 14:47:36 +0000

    Abstracted state management to index page

--- src/components/notables/details.component.js
+++ src/components/notables/details.component.js
@@ -1,25 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import New from "./new.component";
 import Edit from "./edit.component";
-import Button from "react-bootstrap/Button";
 
 // Middle-man component to assess state of index page and decide which sub-component to show
 function Details(props) {
-  const [newRecord, setNewRecord] = useState(false);
-
-  // In some use cases the singular of the `type` prop (ie "item" instead of "items") is needed
-  // This removes the last character from and capitalises the string. While not perfect, it works
-  // for the available notable types.
-  const singularType = () => {
-    return `${props.type.charAt(0).toUpperCase()}${props.type.slice(1, -1)}`;
-  };
-
-  // When selected ID is changed, default to regular view
-  // If a user starts creating a new notable, then selects & deselects an existing one,
-  // this stops them from returning to an empty form
-  useEffect(() => {
-    setNewRecord(false);
-  }, [props.id]);
+  const setNewRecord = props.setNewRecord;
 
   // Decide which panel to show
   if (props.id) {
@@ -31,11 +16,10 @@ function Details(props) {
           retrieveNotables={props.retrieveNotables}
           id={props.id}
           type={props.type}
-          singularType={singularType()}
         />
       </div>
     );
-  } else if (newRecord) {
+  } else if (props.newRecord) {
     return (
       // Otherwise, if the user has clicked the "Add new notable" button, render the new notable form
       <div className="col-md-6">
@@ -43,25 +27,12 @@ function Details(props) {
           setNewRecord={setNewRecord}
           retrieveNotables={props.retrieveNotables}
           notebookId={props.notebookId}
-          type={singularType()}
         />
       </div>
     );
-  } else {
-    return (
-      // By default, just show a button to add a new notable
-      <div className="col-md-6">
-        <Button
-          onClick={() => {
-            setNewRecord(true);
-          }}
-          className="w-100"
-        >
-          Add {singularType()}
-        </Button>
-      </div>
-    );
   }
+
+  return null;
 }
 
 export default Details;
--- src/components/notables/index.component.js
+++ src/components/notables/index.component.js
@@ -4,12 +4,17 @@ import Search from "../search.component";
 import List from "../list.component";
 import NotableDataService from "../../services/notable.service";
 import Details from "./details.component";
+import Button from "react-bootstrap/Button";
+import Helper from "../../helpers/notable_helper";
 
 function Show(props) {
   const { notebookId } = useParams();
 
   const [notables, setNotables] = useState([]);
   const [currentId, setCurrentId] = useState(null);
+
+  // Manage state of new page
+  const [newRecord, setNewRecord] = useState(false);
 
   // Filtered notables
   const [queriedNotables, setQueriedNotables] = useState([]);
@@ -28,6 +33,7 @@ function Show(props) {
     [setNotables, notebookId, props.type]
   );
 
+  // Conditionally render search bar
   const renderSearch = () => {
     if (notables.length) {
       return (
@@ -45,6 +51,18 @@ function Show(props) {
     retrieveNotables();
   }, [retrieveNotables]);
 
+  // Event handler for switching to "New" page
+  const showNew = () => {
+    setNewRecord(true);
+    setCurrentId(null);
+  };
+
+  // Set current ID & reset new page view
+  const showItem = (id) => {
+    setNewRecord(false);
+    setCurrentId(id);
+  };
+
   return (
     <div className="list row">
       <div className="col-md-6">
@@ -52,9 +70,18 @@ function Show(props) {
 
         {renderSearch()}
 
+        <div className="col-md-6">
+          <Button
+            onClick={showNew}
+            className="w-100"
+          >
+            Add {Helper.singular(props.type)}
+          </Button>
+        </div>
+
         <List
           currentId={currentId}
-          setCurrentId={setCurrentId}
+          setCurrentId={showItem}
           items={queriedNotables}
           label="name"
         />
@@ -65,6 +92,8 @@ function Show(props) {
         retrieveNotables={retrieveNotables}
         type={props.type}
         notebookId={notebookId}
+        newRecord={newRecord}
+        setNewRecord={setNewRecord}
       />
     </div>
   );
