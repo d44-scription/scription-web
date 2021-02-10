92e209cc6264b855107bb335abf815eb5f1518e6
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-10 14:20:39 +0000

    Added first look at authentication service

---
+++ src/services/authentication.service.js
@@ -0,0 +1,46 @@
+import http from "../http-common";
+
+class AuthenticationDataService {
+  login(email, password) {
+    let params = {};
+    params["user"] = {};
+
+    params["user"]["email"] = email;
+    params["user"]["password"] = password;
+
+    http
+      .post("/api/v1/users/login", params)
+      .then((response) => {
+        if (response.data.token) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
+        return response.data;
+      })
+      .catch((e) => {
+        // Handle errors
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  register(displayName, email, password, passwordConfirmation) {
+    let params = {};
+    params["user"] = {};
+
+    params["user"]["display_name"] = displayName;
+    params["user"]["email"] = email;
+    params["user"]["password"] = password;
+    params["user"]["password_confirmation"] = passwordConfirmation;
+
+    return http.post("/api/v1/users", params);
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
+}
+
+export default new AuthenticationDataService();
