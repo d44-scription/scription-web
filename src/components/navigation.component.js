905c1eb487418354e373098b2d851826e67c8f62
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-14 21:26:55 +0000

    Updated navigation component to incorporate logout action

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { useLocation, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import Breadcrumbs from "./breadcrumbs.component";
 import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
 import AuthenticationDataService from "../services/authentication.service";
 
 function Navigation(props) {
+  const history = useHistory();
+
+  const logout = () => {
+    AuthenticationDataService.logout()
+    history.push("/")
+  }
+
   return (
     <Nav className="bg-primary">
       <Breadcrumbs />
 
-      {AuthenticationDataService.loggedIn() ? (
-        <p className="text-dark">Signed in</p>
-      ) : (
-        <p className="text-dark">Signed out</p>
-      )}
+      {/* TODO: Figure out conditional rendering */}
+      <Button onClick={logout}>
+        Log out
+      </Button>
     </Nav>
   );
 }
