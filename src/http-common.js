071d8d64e619d04c5f036b3fa4981f907bb31a92
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-15 17:32:15 +0000

    Added axios interceptor to return to login page when request is unauthorised

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 
-export default axios.create({
+const http = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-type": "application/json",
   },
 });
+
+http.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response.status === 401) {
+      window.location.href = "/";
+    }
+  }
+);
+
+export default http;
